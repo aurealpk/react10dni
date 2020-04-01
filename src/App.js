b3aaadd7ea30fa5cb6816ad979129f69a683d9ef
Paweł Karczewski <au.pawel@gmail.com>
2020-04-01 22:59:54 +0200

    exporty i importy komponenty funkcyjne

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => (
+    <div className="warapper">
+        <h1 className="mainHeader">Hello World</h1>
+        <h2 className="secondaryHeader">Hello eduweb</h2>
+    </div>
+);
+
+export default App;
